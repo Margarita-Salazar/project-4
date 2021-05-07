76d50f098b791a0d3d78ea7e43fa97150281a809
unknown <margarita.salazar.mls@gmail.com>
2021-05-07 01:47:43 -0500

    setting up api

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT
+server.listen(PORT, ()=>{
+    console.log(`listening to ${PORT}`)
+})
