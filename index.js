1376be46dcd0288612b1d498b6906e09bb178400
unknown <margarita.salazar.mls@gmail.com>
2021-05-07 02:06:12 -0500

    WIP

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const server = express();
 
 const PORT = process.env.PORT
+
+server.use(express.json());
+
+server.use('*', (req,res)=>{
+    res.send('<h1>Hello World</h1>')
+})
 server.listen(PORT, ()=>{
     console.log(`listening to ${PORT}`)
 })
